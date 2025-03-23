e7169087a454576b14c01aeb3dbf4e76e1ddfa05
DangToVu <vuminhdang811@gmail.com>
2025-03-23 14:49:37 +0700

    update for auto move to next level

--- Assets/Scripts/Trophy/Trophy.cs
+++ Assets/Scripts/Trophy/Trophy.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement; // Import SceneManager
 
 public class Trophy : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class Trophy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
             anim.SetTrigger("Win");
             if (audioSource != null && !audioSource.isPlaying)
@@ -22,19 +23,26 @@ public class Trophy : MonoBehaviour
                 audioSource.Play();
             }
 
-            
-            StartCoroutine(QuitGameAfterDelay(5f));
+            StartCoroutine(LoadNextLevelAfterDelay(5f));
         }
     }
 
-    IEnumerator QuitGameAfterDelay(float delay)
+    IEnumerator LoadNextLevelAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
-       
-        Application.Quit();
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
+        // Lấy tên scene hiện tại
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Kiểm tra nếu còn level tiếp theo thì load, nếu hết thì về level 1
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0); // Nếu hết level, quay về level đầu
+        }
     }
 }
